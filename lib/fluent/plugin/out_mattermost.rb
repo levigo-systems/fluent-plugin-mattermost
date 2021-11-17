181a3fab2faf38f762f88f616f76324a0cf568de
Pigi <p.minardi@levigo.de>
2021-11-17 14:36:54 +0100

    added the message_keys param + fetch_keys method added

--- lib/fluent/plugin/out_mattermost.rb
+++ lib/fluent/plugin/out_mattermost.rb
@@ -22,6 +22,10 @@ module Fluent
 
       config_param :message, :string, default: nil
 
+      config_param :message_keys, default: nil do |val|
+        val.split(',')
+      end
+
       config_param :enable_tls,  :bool, default: true
 
       def configure(conf)
@@ -100,9 +104,24 @@ module Fluent
       end
 
       def build_message(record)
-        @message % record.to_json
+        if @message_keys != nil
+          values = fetch_keys(record, @message_keys)
+          @message % values.to_json
+        else
+          @message % record.to_json
+        end
       end
 
+      def fetch_keys(record, keys)
+        Array(keys).map do |key|
+          begin
+            record.fetch(key, "out_mattermost: the specified message_key '#{key}' not found in record. [#{record}]").to_s
+          rescue KeyError
+            log.warn "out_mattermost: the specified message_key '#{key}' not found in record. [#{record}]"
+            ''
+          end
+        end
+      end
     end
   end
 end
